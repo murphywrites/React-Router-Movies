7e3788905c51c17d7c5d3bb3f808640abb1f957b
Michael Murphy <mmurph917@michaels-mbp.lan>
2020-12-14 18:41:01 -0600

    Met MVP

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,20 +1,31 @@
 import React from 'react';
+import { Link, useRouteMatch, useParams, useHistory } from 'react-router-dom'
 
 export default function MovieList(props) {
+  const { url } = useRouteMatch();
+  const {movies} = props
+ 
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        // <Link to={`/movies/${movie.id}`}>
+          <MovieDetails key={movie.id} movie={movie}/>
+        // </Link>
       ))}
     </div>
   );
 }
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
+  const { title, director, metascore, id} = props.movie;
+  const  history = useHistory();
+  console.log(history)
+  // const {history} = props;
 
   return (
-    <div className="movie-card">
+    <div className="movie-card"  onClick = {() => {
+      history.push(`/movies/${id}`)
+    }} >
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
